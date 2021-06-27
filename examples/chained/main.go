000b4428a4d5647d6c275ff547ae1d28625c8ad6
Radhi Fadlillah <m.radhi.f@gmail.com>
2021-06-27 21:59:45 +0700

    Add example for chaining document between extractors

---
+++ examples/chained/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	fp "path/filepath"
+	"time"
+
+	"github.com/go-shiori/dom"
+	"github.com/go-shiori/go-readability"
+	distiller "github.com/markusmobius/go-domdistiller"
+	"github.com/markusmobius/go-htmldate"
+	"github.com/markusmobius/go-trafilatura"
+	"golang.org/x/net/html"
+)
+
+func main() {
+	// Find file names
+	filePaths, err := getFileList()
+	checkError(err)
+
+	// Process each path
+	var nDocument int
+	var parseTime time.Duration
+	var readabilityTime time.Duration
+	var domDistillerTime time.Duration
+	var trafilaturaTime time.Duration
+	var dateTime time.Duration
+
+	for _, path := range filePaths {
+		nDocument++
+
+		// Parse file
+		start := time.Now()
+		doc, err := parseFile(path)
+		checkError(err)
+		parseTime += time.Now().Sub(start)
+
+		// Use readability
+		start = time.Now()
+		readability.FromDocument(doc, nil)
+		readabilityTime += time.Now().Sub(start)
+
+		// Use dom distiller
+		start = time.Now()
+		distillerOpts := &distiller.Options{SkipPagination: true}
+		distiller.Apply(doc, distillerOpts)
+		domDistillerTime += time.Now().Sub(start)
+
+		// Use trafilatura
+		start = time.Now()
+		trafilaturaOpts := trafilatura.Options{NoFallback: false}
+		trafilatura.ExtractDocument(doc, trafilaturaOpts)
+		trafilaturaTime += time.Now().Sub(start)
+
+		// Use html date
+		start = time.Now()
+
+		// Last modified date
+		dateOpts := htmldate.Options{}
+		htmldate.FromDocument(doc, dateOpts)
+
+		// Publish date
+		dateOpts.UseOriginalDate = true
+		htmldate.FromDocument(doc, dateOpts)
+		dateTime += time.Now().Sub(start)
+	}
+
+	// Print message
+	parseDuration := parseTime.Seconds()
+	readabilityDuration := readabilityTime.Seconds()
+	domDistillerDuration := domDistillerTime.Seconds()
+	trafilaturaDuration := trafilaturaTime.Seconds()
+	dateDuration := dateTime.Seconds()
+	totalDuration := parseDuration + readabilityDuration +
+		domDistillerDuration + trafilaturaDuration + dateDuration
+
+	parseSpeed := float64(nDocument) / parseDuration
+	readabilitySpeed := float64(nDocument) / readabilityDuration
+	domDistillerSpeed := float64(nDocument) / domDistillerDuration
+	trafilaturaSpeed := float64(nDocument) / trafilaturaDuration
+	dateSpeed := float64(nDocument) / dateDuration
+	avgSpeed := float64(nDocument) / totalDuration
+
+	fmt.Printf("N document : %d\n", nDocument)
+	fmt.Printf("Parsing    : %.3f s (%.3f doc/s)\n", parseDuration, parseSpeed)
+	fmt.Printf("Readability: %.3f s (%.3f doc/s)\n", readabilityDuration, readabilitySpeed)
+	fmt.Printf("Distiller  : %.3f s (%.3f doc/s)\n", domDistillerDuration, domDistillerSpeed)
+	fmt.Printf("Trafilatura: %.3f s (%.3f doc/s)\n", trafilaturaDuration, trafilaturaSpeed)
+	fmt.Printf("HtmlDate   : %.3f s (%.3f doc/s)\n", dateDuration, dateSpeed)
+	fmt.Printf("Total      : %.3f s (%.3f doc/s)\n", totalDuration, avgSpeed)
+
+}
+
+func getFileList() ([]string, error) {
+	var filePaths []string
+	err := fp.Walk("test-files", func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() && fp.Ext(path) == ".html" {
+			filePaths = append(filePaths, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return filePaths, nil
+}
+
+func parseFile(path string) (*html.Node, error) {
+	// Open file
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return dom.Parse(f)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,14 +6,15 @@ require (
 	github.com/abadojack/whatlanggo v1.0.1
 	github.com/beevik/etree v1.1.0
 	github.com/go-shiori/dom v0.0.0-20210627111528-4e4722cd0d65
-	github.com/go-shiori/go-readability v0.0.0-20210528061711-a4ef7bdf2420
-	github.com/markusmobius/go-domdistiller v0.0.0-20210520100406-78c3172602a8
+	github.com/go-shiori/go-readability v0.0.0-20210627123243-82cc33435520
+	github.com/markusmobius/go-domdistiller v0.0.0-20210627124457-88889a6bc85d
+	github.com/markusmobius/go-htmldate v0.0.0-20210627113013-f9a1cbfd7762
 	github.com/matoous/go-nanoid/v2 v2.0.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/spf13/cobra v1.0.0
-	github.com/stretchr/testify v1.6.1
+	github.com/spf13/cobra v1.1.3
+	github.com/stretchr/testify v1.7.0
 	github.com/yosssi/gohtml v0.0.0-20201013000340-ee4748c638f4
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
-	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4
-	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 )
